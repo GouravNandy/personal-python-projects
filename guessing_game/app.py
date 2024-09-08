3ec61acbd6c38b327e102d56c6eb2e2b4181c2c5
Gourav <career.gourav@gmail.com>
2024-09-08 23:47:58 +0530

    2nd commit

--- guessing_game/app.py
+++ guessing_game/app.py
@@ -1,7 +1,9 @@
-import math
-import numpy as np
+# importing necessary libraries
+import math  # for the log calculation in the further code
+import numpy as np  # for generating the random number
 
 if __name__ == '__main__':
+    # General Welcome statement and input for range by the user. Handled using Exception handled for datatype cases
     print('Welcome! This is the new Guessing game. Lets see how strong your intuition is.')
     print('Choose the range (lower bound and upper bound respectively). Both should be integers separated by a space :  ')
     while True:
@@ -12,11 +14,18 @@ if __name__ == '__main__':
             continue
         else:
             break
+    # Calculating the number of chances
     chances = int(math.log2(u-l+1))
+
+    # Generating the random number
     r = np.random.randint(l, u)
     print(f'I have generated the number for you to guess. You have {chances} chances to guess the number. ')
+
+    # Initializing the count value to check the number of turns completed and flag value if the user has chosen the right guess number.
     c = 0
     f = 0
+
+    # The actual game
     while c <= chances:
         while True:
             try:
@@ -42,6 +51,7 @@ if __name__ == '__main__':
         if c == chances:
             print('Last chance Buddy !!')
 
+    # Conclusion statement from the flag value
     if f != 1:
         print('Sorry! Better run the program and try again.')
     else:
